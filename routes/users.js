499e5614f1dd93ebfaf2ad78b9faabfd53f42476
kaleemshabir <kaleemu3329@gmail.com>
2021-06-12 22:24:42 +0500

    up

--- routes/users.js
+++ routes/users.js
@@ -17,7 +17,7 @@ const { protect, authorize } = require('../middleware/auth');
 router.use(protect);
 // router.use(authorize('admin'));
 
-router.route('/').get(protect("admin"), advancedResults(User), getUsers).post(createUser);
+router.route('/').get(authorize("admin"), advancedResults(User), getUsers).post(createUser);
 
 router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
 
