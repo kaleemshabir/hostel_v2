da9d6c3baf41ecb61c6e5a571117612c80b30f38
kaleemshabir <kaleemu3329@gmail.com>
2021-07-13 15:39:24 +0500

    up

--- models/Shop.js
+++ models/Shop.js
@@ -3,7 +3,6 @@ const ShopSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please add a name'],
-    unique: true,
     trim: true,
     maxlength: [50, 'Name cannot be more than 50 characters'],
   },
