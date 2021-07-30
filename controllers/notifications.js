d8d32fcd5a9643331c6b8743cf63e9835a4dc79a
kaleemshabir <kaleemu3329@gmail.com>
2021-07-30 18:19:20 +0500

    sort notification

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -2,7 +2,8 @@ const Notification = require("../models/Notification");
 exports.getNotifications = async (req, res) => {
   const notifications = await Notification.find({ publisher: req.user.id })
     .populate("user", "name email photo contactNumber")
-    .select("-publisher");
+    .select("-publisher")
+    .sort([['createdAt', -1]]);
   return res.status(200).json({
     success: true,
     message:
