1be174ffd3ff46048d2cadf9276dee7321ee9452
kaleemshabir <kaleemu3329@gmail.com>
2021-08-04 11:49:21 +0500

    update

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -87,7 +87,7 @@ exports.addRoom = asyncHandler(async (req, res, next) => {
   }
   let room = await Room.findOne({ hostel: req.params.hostelId, roomNumber:req.body.roomNumber });
   if(room) {
-    return next(new ErrorResponse("This room# already exists, Please choose another number#"),400)
+    return next(new ErrorResponse("This room# already exists, Please choose another number#", 400));
   }
 
 
